883bedfd42199e3138a1c34c4261c1458adf872a
Sammydepoj <samueladepoju86@yahoo.com>
2023-01-11 14:51:01 +0100

    Feat: Completed flexible section for desktop view

--- src/components/Flexible/Flexible.jsx
+++ src/components/Flexible/Flexible.jsx
@@ -4,26 +4,32 @@ import image1 from "./Assets/image1.png";
 import image2 from "./Assets/image2.png";
 import image3 from "./Assets/image3.png";
 import image4 from "./Assets/image4.png";
+import Button from "../Button/Button";
 
 const Flexible = () => {
   return (
     <div className={styles.flexible}>
       <div className={styles.flexibleImages}>
-        <div className={styles.imgContainer1}>
-          <p>Flexible Leases</p>
-          {/* <img src={image1} alt="avalable home" /> */}
+        <div className={styles.mainImgContainer}>
+          <div className={styles.imgContainer1}>
+            <p>Flexible Leases</p>
+            <img src={image1} alt="avalable home" />
+          </div>
+          <div className={styles.imgContainer2}>
+            <p>7-Day Happiness Guaranteed</p>
+            <img src={image2} alt="avalable home" />
+          </div>
         </div>
-        <div className={styles.imgContainer2}>
-          <p>7-Day Happiness Guaranteed</p>
-          {/* <img src={image2} alt="avalable home" /> */}
-        </div>
-        <div className={styles.imgContainer3}>
-          <p>Monthly House Cleaning</p>
-          {/* <img src={image3} alt="avalable home" /> */}
-        </div>
-        <div className={styles.imgContainer4}>
-          <p>Choose Your Own Roommate</p>
-          {/* <img src={image4} alt="avalable home" /> */}
+
+        <div className={styles.mainImgContainer}>
+          <div className={styles.imgContainer3}>
+            <p>Monthly House Cleaning</p>
+            <img src={image3} alt="avalable home" />
+          </div>
+          <div className={styles.imgContainer4}>
+            <p>Choose Your Own Roommate</p>
+            <img src={image4} alt="avalable home" />
+          </div>
         </div>
       </div>
       <div className={styles.flexibleText}>
@@ -36,6 +42,7 @@ const Flexible = () => {
           roommate finder option. We provide you the flexibility that you most
           desire.
         </p>
+        <Button value={"Search Rooms"}></Button>
       </div>
     </div>
   );
