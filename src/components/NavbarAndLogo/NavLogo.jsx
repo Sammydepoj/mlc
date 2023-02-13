f2b1d7a28b4e29d2ff2443f8ac4a140b8baf2dc2
Adepoju Adeyemi Samuel <samueladepoju86@yahoo.com>
2023-02-13 16:34:26 +0100

    harmbuger feature implemented and in progess

--- src/components/NavbarAndLogo/NavLogo.jsx
+++ src/components/NavbarAndLogo/NavLogo.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navlogo.module.css";
 import logo from "./assets/logo.svg";
 
+// const closeMenu = () => {
+//   let nav = document.getElementById("nav");
+//   nav.style.display = "none";
+// };
+
 const NavLogo = () => {
+  const [MenuShow, setMenuShow] = useState(false);
+
+  const showMenu = () => {
+    setMenuShow(true);
+    let nav = document.getElementById("nav");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  };
+
   return (
     <div className={styles.navlogo}>
       <div className="logo">
         <img src={logo} alt="page logo" />
       </div>
       <div className="nav">
-        <ul className={styles.ul}>
+        <ul className={styles.ul} id="nav">
           <li className={styles.li}>
             <a href="#">Home</a>
           </li>
@@ -23,9 +40,11 @@ const NavLogo = () => {
             <a href="#">Contact Us</a>
           </li>
         </ul>
+        <button className={styles.harmburger} onClick={showMenu}>
+          Open Modal
+        </button>
       </div>
     </div>
   );
 };
-
 export default NavLogo;
